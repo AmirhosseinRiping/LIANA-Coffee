b7fb1778f0cba0151a994199d56dc4d265fd3969
AmirhosseinRiping <amirhoseinest2@gmail.com>
2024-01-28 10:54:07 +0330

    add page profile

--- assets/js/main.js
+++ assets/js/main.js
@@ -400,3 +400,12 @@
   // --------------------------------------------------
 
 })(jQuery);
+
+
+$(function (){
+  $("#fileupload").change(function (event) {
+    var x = URL.createObjectURL(event.target.files[0]);
+    $("#upload-imge").attr("src", x);
+    console.log(event);
+  })
+})
